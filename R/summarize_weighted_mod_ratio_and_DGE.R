7a200efb56769f5b02100c402d793ea83ec8f41b
Hanna Lee <hannalee809@gmail.com>
2025-06-05 14:17:46 -0400

    Edited .RD files for updated functions

--- R/summarize_weighted_mod_ratio_and_DGE.R
+++ R/summarize_weighted_mod_ratio_and_DGE.R
@@ -1,40 +1,53 @@
 #' Summarize Weighted Modification Ratio and DGE and Provide annotations for clusters
 #'
-#' This function filters m6A sites from m6anet output based on probability_modified criteria.
+#' This function groups transcripts into functional categories based on gene expression and m6A methylation changes,
+#' and summarizes transcript biotype and regional site distributions.
 #'
 #' @param df Output from weighted_mod_ratio_and_DGE
-#' @param group_1_name Name for group 1
-#'  @param group_2_name Name for group 2
-#' @return filtered dataframe of m6A sites
-#' @importFrom dplyr group_by summarize mutate n bind_rows
-#' @importFrom magrittr %>%
-#' @importFrom rlang sym !!
+#' @param group1_name Name for group 1
+#' @param group2_name Name for group 2
+#' @param log2FC_dge Column name for log2 fold change (DGE)
+#' @param log2fc_wmr Column name for log2 fold change (weighted mod ratio)
+#' @param sig_col Column name for significance boolean (e.g., adjusted p < 0.05)
+#' @param combine Logical; whether to return combined summaries
+#'
+#' @return A list containing filtered data frames and summaries
 #' @export
 
-summarize_weighted_mod_ratio_and_DGE <- function(df, group1_name = "Group1", group2_name = "Group2", log2FC_dge, log2fc_wmr, sig_col = "significant", combine = TRUE) {
+summarize_weighted_mod_ratio_and_DGE <- function(df,
+                                                 group1_name = "Group1",
+                                                 group2_name = "Group2",
+                                                 log2FC_dge,
+                                                 log2fc_wmr,
+                                                 sig_col = "significant",
+                                                 combine = TRUE) {
   group_list <- list()
   all_biotype <- list()
   all_region <- list()
 
-  # Define filtering rules for each group
+  # Use tidy evaluation to extract columns dynamically
+  log2FC_dge <- rlang::ensym(log2FC_dge)
+  log2fc_wmr <- rlang::ensym(log2fc_wmr)
+  sig_col <- rlang::ensym(sig_col)
+
+  # Define filtering rules as expressions
   filters <- list(
-    upreg_hyperm6A_group1        = sig_col == TRUE  & log2FC_dge > 0 & log2fc_wmr > 0,
-    upreg_group1_hyperm6a_group2 = sig_col == TRUE  & log2FC_dge > 0 & log2fc_wmr < 0,
-    upreg_hyperm6A_group2        = sig_col == TRUE  & log2FC_dge < 0 & log2fc_wmr < 0,
-    upreg_group2_hyperm6a_group1 = sig_col == TRUE  & log2FC_dge < 0 & log2fc_wmr > 0,
-    noDGE_hyperm6A_group1        = sig_col == FALSE & log2fc_wmr > 0,
-    noDGE_hyperm6A_group2        = sig_col == FALSE & log2fc_wmr < 0
+    upreg_hyperm6A_group1        = rlang::expr((!!sig_col) == TRUE  & (!!log2FC_dge) > 0 & (!!log2fc_wmr) > 0),
+    upreg_group1_hyperm6a_group2 = rlang::expr((!!sig_col) == TRUE  & (!!log2FC_dge) > 0 & (!!log2fc_wmr) < 0),
+    upreg_hyperm6A_group2        = rlang::expr((!!sig_col) == TRUE  & (!!log2FC_dge) < 0 & (!!log2fc_wmr) < 0),
+    upreg_group2_hyperm6a_group1 = rlang::expr((!!sig_col) == TRUE  & (!!log2FC_dge) < 0 & (!!log2fc_wmr) > 0),
+    noDGE_hyperm6A_group1        = rlang::expr((!!sig_col) == FALSE & (!!log2fc_wmr) > 0),
+    noDGE_hyperm6A_group2        = rlang::expr((!!sig_col) == FALSE & (!!log2fc_wmr) < 0)
   )
 
   for (name in names(filters)) {
-    group_df <- df[filters[[name]], ]
+    group_df <- dplyr::filter(df, !!filters[[name]])
 
     # Biotype summary
     biotype_summary <- group_df %>%
       dplyr::group_by(gene_biotype) %>%
       dplyr::summarize(count = dplyr::n(), .groups = "drop") %>%
-      dplyr::mutate(frequency = count / sum(count),
-                    group = name)
+      dplyr::mutate(frequency = count / sum(count), group = name)
 
     # Region summary
     region_summary <- tibble::tibble(
@@ -66,7 +79,6 @@ summarize_weighted_mod_ratio_and_DGE <- function(df, group1_name = "Group1", gro
     }
   }
 
-  # If combine = TRUE, return merged summaries too
   if (combine) {
     return(list(
       groups = group_list,
@@ -77,4 +89,3 @@ summarize_weighted_mod_ratio_and_DGE <- function(df, group1_name = "Group1", gro
     return(group_list)
   }
 }
-
